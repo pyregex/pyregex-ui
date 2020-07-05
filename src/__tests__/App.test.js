bea39fd6b0f3b306f4f9154bfbd6cc68dd3daf82
Rodolfo Carvalho <rodolfo.ueg@gmail.com>
2020-07-05 18:43:13 -0400

    Adding a simple test

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,9 @@
+import {render} from '@testing-library/svelte'
+
+import App from '../App.svelte'
+
+test('renders page', () => {
+  const {getByText} = render(App, {name: 'World'});
+
+  expect(getByText('Hello World!')).toBeInTheDocument();
+});
